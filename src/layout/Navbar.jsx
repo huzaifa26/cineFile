4316ee0b4b3bff410e8b71f504fc6aed2258286c
Syed Huzaifa Abid <62389218+huzaifa26@users.noreply.github.com>
2023-04-24 23:16:06 +0500

    fix

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,37 +1,43 @@
 import React from 'react'
-import {BsStar} from "react-icons/bs"
+import { BsStar } from "react-icons/bs"
 
 export default function Navbar() {
   return (
     <div>
-      <div className='w-[92%] m-auto h-[180px] flex items-center justify-between'>
-        <img className='w-[20vw]' src='./Logo.svg'></img>
+      <div className='w-[92%] m-auto h-[100px] flex items-center justify-between'>
+        <img className='w-[180px]' src='./Logo.svg'></img>
         <div className='flex gap-[1.85vw]'>
-          <img className='w-[7.23vw]' src='./profilePic.svg'></img>
-          <img className='w-[1.85vw]' src='./dropDown.svg'></img>
+          <img className='w-[60px]' src='./profilePic.svg'></img>
+          <img className='w-[20px]' src='./dropDown.svg'></img>
         </div>
       </div>
-      <div className='h-[155px] bg-[rgba(229,9,20,1)] flex items-center'>
-        <div className='flex gap-[5.41vw]'>
-          <div className='flex flex-col items-center'>
-              <img src='./rating.svg'/>
+      <div className='h-[80px] bg-[rgba(229,9,20,1)] flex items-center'>
+        <div className='w-[92%] m-auto flex justify-between items-center'>
+          <div className='flex gap-[5.41vw]'>
+            <div className='flex flex-col items-center justify-center'>
+              <img className='w-[35px]' src='./rating.svg' />
               <p>Rating</p>
-          </div>
-          <div className='flex flex-col items-center'>
-              <img src='./files.svg'/>
+            </div>
+            <div className='flex flex-col items-center justify-center'>
+              <img className='w-[30px]' src='./files.svg' />
               <p>File</p>
-          </div>
-          <div className='flex flex-col items-center'>
-              <img src='./theater.svg'/>
+            </div>
+            <div className='flex flex-col items-center justify-center'>
+              <img className='w-[30px]' src='./theater.svg' />
               <p>Theaters</p>
-          </div>
-          <div className='flex flex-col items-center'>
-              <img src='./inbox.svg'/>
+            </div>
+            <div className='flex flex-col items-center justify-center '>
+              <img className='w-[30px]' src='./inbox.svg' />
               <p>Inbox</p>
-          </div>
-          <div className='flex flex-col items-center'>
-              <img src='./feeds.svg'/>
+            </div>
+            <div className='flex flex-col items-center justify-center'>
+              <img className='w-[30px]' src='./feeds.svg' />
               <p>Feed</p>
+            </div>
+          </div>
+          <div className='relative'>
+            <input className='h-[40px] w-[23.18vw] px-[24px] rounded-[6px] text-black' placeholder='Search...'></input>
+            <img className='absolute right-[23px] top-[50%] translate-y-[-50%] w-[25px] cursor-pointer' src='./search.svg'/>
           </div>
         </div>
       </div>
--- src/pages/Rating.jsx
+++ src/pages/Rating.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+
 
 export default function Rating() {
+  const [rangeValue, setRangeValue] = useState(50);
+
+  const handleRangeChange = (event) => {
+    console.log(event.target.value);
+    setRangeValue(event.target.value);
+  }
+
+  const trackStyle = {
+    '--progress': `${rangeValue}%`,
+    background: `linear-gradient(to right, white, white var(--progress), #959595 var(--progress), #959595)`
+  };
+
   return (
-    <div>Rating</div>
+    <div>
+      <div className='h-[250px]' style={{ backgroundImage: "url('./ratingMain.png')" }}>
+        <div className='h-full w-full bg-[rgba(0,0,0,0.9)] flex items-center justify-center'>
+          <h1 className=''>Please Rate The Movie</h1>
+        </div>
+      </div>
+
+      <div className='w-[92%] m-auto'>
+        <div>
+          <input
+            className='w-[50vw] bg-[#959595]'
+            type="range"
+            min="0"
+            max="10"
+            step="0.1"
+            value={rangeValue}
+            onChange={handleRangeChange}
+            style={trackStyle}
+          />
+
+        </div>
+      </div>
+    </div>
   )
 }
