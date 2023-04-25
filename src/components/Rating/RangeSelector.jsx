76454893a94056a9dd5ba924525f758bc53e2f6a
Syed Huzaifa Abid <62389218+huzaifa26@users.noreply.github.com>
2023-04-25 10:15:15 +0500

    completed rating page design

---
+++ src/components/Rating/RangeSelector.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+export default function RangeSelector({ title }) {
+  const [rangeValue, setRangeValue] = useState(10);
+
+  const handleRangeChange = (event) => {
+    setRangeValue(event.target.value);
+  }
+
+  console.log(rangeValue);
+
+  const trackStyle = {
+    '--progress': `${rangeValue * 10}%`,
+    background: `linear-gradient(to right, white, white var(--progress), #959595 var(--progress), #959595)`
+  };
+
+  return (
+    <div className='w-[50vw] mt-[15px]'>
+      <div className='flex justify-between items-center'>
+        <p className='font-[600] text-[24px]'>{title}</p>
+        <p className='font-[500] text-[18px]'>{`${rangeValue}/10`}</p>
+      </div>
+      <input
+        className='w-[50vw] bg-[#959595]'
+        type="range"
+        min="0"
+        max="10"
+        step="0.1"
+        value={rangeValue}
+        onChange={handleRangeChange}
+        style={trackStyle}
+      />
+    </div>
+  )
+}
--- src/pages/Rating.jsx
+++ src/pages/Rating.jsx
@@ -1,20 +1,9 @@
 import React, { useState } from 'react'
+import RangeSelector from '../components/Rating/RangeSelector'
 
 
 
 export default function Rating() {
-  const [rangeValue, setRangeValue] = useState(50);
-
-  const handleRangeChange = (event) => {
-    setRangeValue(event.target.value);
-  }
-
-  console.log(rangeValue);
-
-  const trackStyle = {
-    '--progress': `${rangeValue*10}%`,
-    background: `linear-gradient(to right, white, white var(--progress), #959595 var(--progress), #959595)`
-  };
 
   return (
     <div>
@@ -24,19 +13,33 @@ export default function Rating() {
         </div>
       </div>
 
-      <div className='w-[92%] m-auto'>
-        <div>
-          <input
-            className='w-[50vw] bg-[#959595]'
-            type="range"
-            min="0"
-            max="10"
-            step="0.1"
-            value={rangeValue}
-            onChange={handleRangeChange}
-            style={trackStyle}
-          />
-
+      <div className='w-[92%] m-auto mt-[34px] flex justify-between'>
+        <div className='flex flex-col'>
+          <RangeSelector title={"Story"} />
+          <RangeSelector title={"Acting"} />
+          <RangeSelector title={"Pacing"} />
+          <RangeSelector title={"Cinematography"} />
+          <RangeSelector title={"Sound EX / Music"} />
+          <RangeSelector title={"Finale"} />
+          <RangeSelector title={"Feelings"} />
+          <button className='text-black bg-white w-[13.64vw] rounded-[4px] h-[40px] min-w-[200px] mt-[20px] block font-[400] text-[18px]'>+ Sub categories</button>
+          <button className='text-white bg-[red] w-[8.51vw] rounded-[4px] h-[48px] min-w-[160px] mt-[20px] block self-end font-[600] text-[18px]'>Calculate</button>
+        </div>
+        <div className='w-[28.22vw] mt-[80px]'>
+          <div className='relative'>
+            <input className='h-[40px] w-[100%] px-[24px] rounded-[6px] text-black' placeholder='Search Movie'></input>
+            <img className='absolute right-[23px] top-[50%] translate-y-[-50%] w-[25px] cursor-pointer' src='./search.svg' />
+          </div>
+          <div className='flex flex-col justify-center items-center mt-[20px]'>
+            <img className='w-[100%]' src='./ratingMoviePoster.png'/>
+            <div className='w-full'>
+              <div className='flex justify-between px-2 mt-[10px]'>
+                <p className='text-[18px] font-[600] leading-[30px]'>Joker</p>
+                <p className='text-[18px] font-[600] leading-[30px]'>Score</p>
+              </div>
+              <p className='text-[16px] text-[#959595] font-[400] leading-[30px] mt-[-5px] px-2'>1hr 15min | 23 mar,2023</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
