8910d999123c84820d3e307d89f1808aaf52f5a5
Syed Huzaifa Abid <62389218+huzaifa26@users.noreply.github.com>
2023-04-25 10:45:53 +0500

    added sub category model

---
+++ src/components/Rating/SubCategoryModal.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { ImCross } from 'react-icons/im'
+
+export default function SubCategoryModal({hideSubCategoryModal}) {
+  return (
+    <div onClick={hideSubCategoryModal} className='w-full h-full z-[100] fixed top-0 left-0 bg-[rgba(0,0,0,0.8)] backdrop-blur-[4px] flex justify-center items-center'>
+      <div onClick={(e)=> e.stopPropagation()} className='w-[56vw] h-[480px] bg-white'>
+        <div className='relative w-full h-[90px] bg-[rgba(229,9,20,0.2)] flex items-center justify-center'>
+          <p className='text-black font-[600] text-[24px]'>Add Sub Category</p>
+          <ImCross onClick={hideSubCategoryModal} className='absolute right-[6%] top-[50%] translate-y-[-50%] text-[red] text-2xl cursor-pointer'/>
+        </div>
+        <div>
+          <select className='border-[1px] border-[red] text-[18px] text-[#959595] w-[24.5vw] h-[45px] rounded-[4px] relative left-[50%] translate-x-[-50%] top-[10vh]'>
+            <option disabled selected value={'null'}>Select Sub-category *</option>
+            <option value={'Cool Factor'}>Cool Factor</option>
+            <option value={'Action'}>Action</option>
+            <option value={'Drama'}>Drama</option>
+            <option value={'Scariness'}>Scariness</option>
+            <option value={'Excitement'}>Excitement</option>
+            <option value={'Suspense'}>Suspense</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/Rating.jsx
+++ src/pages/Rating.jsx
@@ -1,47 +1,63 @@
 import React, { useState } from 'react'
 import RangeSelector from '../components/Rating/RangeSelector'
+import SubCategoryModal from '../components/Rating/SubCategoryModal'
 
 
 
 export default function Rating() {
 
-  return (
-    <div>
-      <div className='h-[250px]' style={{ backgroundImage: "url('./ratingMain.png')" }}>
-        <div className='h-full w-full bg-[rgba(0,0,0,0.9)] flex items-center justify-center'>
-          <h1 className=''>Please Rate The Movie</h1>
-        </div>
-      </div>
+  const [subCategoryModal, setSubCategoryModal] = useState(false);
 
-      <div className='w-[92%] m-auto mt-[34px] flex justify-between'>
-        <div className='flex flex-col'>
-          <RangeSelector title={"Story"} />
-          <RangeSelector title={"Acting"} />
-          <RangeSelector title={"Pacing"} />
-          <RangeSelector title={"Cinematography"} />
-          <RangeSelector title={"Sound EX / Music"} />
-          <RangeSelector title={"Finale"} />
-          <RangeSelector title={"Feelings"} />
-          <button className='text-black bg-white w-[13.64vw] rounded-[4px] h-[40px] min-w-[200px] mt-[20px] block font-[400] text-[18px]'>+ Sub categories</button>
-          <button className='text-white bg-[red] w-[8.51vw] rounded-[4px] h-[48px] min-w-[160px] mt-[20px] block self-end font-[600] text-[18px]'>Calculate</button>
-        </div>
-        <div className='w-[28.22vw] mt-[80px]'>
-          <div className='relative'>
-            <input className='h-[40px] w-[100%] px-[24px] rounded-[6px] text-black' placeholder='Search Movie'></input>
-            <img className='absolute right-[23px] top-[50%] translate-y-[-50%] w-[25px] cursor-pointer' src='./search.svg' />
+  const showSubCategoryModal=()=>{
+    setSubCategoryModal(true)
+  }
+  const hideSubCategoryModal=()=>{
+    setSubCategoryModal(false)
+
+  }
+
+  return (
+    <>
+      {subCategoryModal &&
+        <SubCategoryModal hideSubCategoryModal={hideSubCategoryModal}/>
+      }
+      <div>
+        <div className='h-[250px]' style={{ backgroundImage: "url('./ratingMain.png')" }}>
+          <div className='h-full w-full bg-[rgba(0,0,0,0.9)] flex items-center justify-center'>
+            <h1 className=''>Please Rate The Movie</h1>
           </div>
-          <div className='flex flex-col justify-center items-center mt-[20px]'>
-            <img className='w-[100%]' src='./ratingMoviePoster.png'/>
-            <div className='w-full'>
-              <div className='flex justify-between px-2 mt-[10px]'>
-                <p className='text-[18px] font-[600] leading-[30px]'>Joker</p>
-                <p className='text-[18px] font-[600] leading-[30px]'>Score</p>
+        </div>
+
+        <div className='w-[92%] m-auto mt-[34px] flex justify-between'>
+          <div className='flex flex-col'>
+            <RangeSelector title={"Story"} />
+            <RangeSelector title={"Acting"} />
+            <RangeSelector title={"Pacing"} />
+            <RangeSelector title={"Cinematography"} />
+            <RangeSelector title={"Sound EX / Music"} />
+            <RangeSelector title={"Finale"} />
+            <RangeSelector title={"Feelings"} />
+            <button onClick={showSubCategoryModal} className='text-black bg-white w-[13.64vw] rounded-[4px] h-[40px] min-w-[200px] mt-[20px] block font-[400] text-[18px]'>+ Sub categories</button>
+            <button className='text-white bg-[red] w-[8.51vw] rounded-[4px] h-[48px] min-w-[160px] mt-[20px] block self-end font-[600] text-[18px]'>Calculate</button>
+          </div>
+          <div className='w-[28.22vw] mt-[80px]'>
+            <div className='relative'>
+              <input className='h-[40px] w-[100%] px-[24px] rounded-[6px] text-black' placeholder='Search Movie'></input>
+              <img className='absolute right-[23px] top-[50%] translate-y-[-50%] w-[25px] cursor-pointer' src='./search.svg' />
+            </div>
+            <div className='flex flex-col justify-center items-center mt-[20px]'>
+              <img className='w-[100%]' src='./ratingMoviePoster.png' />
+              <div className='w-full'>
+                <div className='flex justify-between px-2 mt-[10px]'>
+                  <p className='text-[18px] font-[600] leading-[30px]'>Joker</p>
+                  <p className='text-[18px] font-[600] leading-[30px]'>Score</p>
+                </div>
+                <p className='text-[16px] text-[#959595] font-[400] leading-[30px] mt-[-5px] px-2'>1hr 15min | 23 mar,2023</p>
               </div>
-              <p className='text-[16px] text-[#959595] font-[400] leading-[30px] mt-[-5px] px-2'>1hr 15min | 23 mar,2023</p>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
